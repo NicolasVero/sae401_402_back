6f94417691caf7685492a8f91a2e42344294d490
Nicolas Vero <nicolas.vero@univ-rouen.fr>
2023-03-23 10:47:47 +0100

    Ajout de la page de traitement des pages

--- back/scripts/creation_pages.php
+++ back/scripts/creation_pages.php
@@ -11,37 +11,9 @@
 
     <?php
 
-    include 'verif_session.php';
-    include 'utilitaire.php';
-    
-    if(isset($_POST['textarea'])) {
-
-        include '../classes/GeneratePage.php';
-        include '../classes/Page.php';
-        
-        $files = getImagesFiles(scandir('../pages/img_temp'));
-        $texte = str_replace('<img src="../pages/img_temp/', '<img src="./images/', $_POST['textarea']);
-        
-        $gp = new GeneratePage($_POST['titre'], "index", $texte);
-        
-        if($gp->generateFolder()) {
-        
-            $p = new Page($gp->getDossier(), "index", $texte, $_POST['type'], $gp->getUrl(), "style.css", $_POST['auteur']);
-            $gp->generateImagesFolder();
-            $p->remplir_bdd();
-            $p->getResume();
-            $gp->generateHTMLFile();
-
-            foreach($files as $file) {
-                if(!moveFile("../pages/img_temp/$file", "../pages/" . $_POST['titre'] . "/images/$file")) {
-                    break;
-                }
-            }
-
-            header('Location: ../accueil.php');
-        }    
-    }
-
+        include 'verif_session.php';
+        include 'utilitaire.php';
+        include 'traitement_creation_pages.php';
 
     ?>
 
